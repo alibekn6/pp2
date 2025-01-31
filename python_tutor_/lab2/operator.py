13be8271ae5308792b9b83f40184a4850db748c9
Alibek Anuarbek <alibekanuarbek@MacBook-Air--Alibek.local>
2025-01-31 22:03:38 +0500

    operator

---
+++ python_tutor_/lab2/operator.py
@@ -0,0 +1,40 @@
+print(10 + 5)
+
+a = 10
+b = 2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+a += b
+a -= b
+a /= b
+a *= b
+a %= b
+a //= b
+a **= b
+a &= b
+a |= b
+a ^= b
+a >>= b
+a <<= b
+
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+
+
+
+print((6 + 3) - (6 + 3))
+
+print(100 + 5 * 3)
