df43408db49db2190ebef3b3717c5d6f32a31c2d
Alibek <alibekandanuarbek@gmail.com>
2025-03-14 18:39:39 +0500

    feat:clock v1.0.0

--- python_tutor_/lab7/main.py
+++ python_tutor_/lab7/main.py
@@ -1,34 +1,54 @@
 import pygame
-
+import time
+import math
 pygame.init()
-screen = pygame.display.set_mode((800, 800))
-done = False
-is_blue = True
-colorRgb = ()
-x = 30
-y = 30
+width = 800
+height = 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Clock")
 
-while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                        is_blue = not is_blue
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_r:
-                        colorRgb = (255, 0, 0)
-            
-        pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_UP]: y -= 1
-        if pressed[pygame.K_DOWN]: y += 1
-        if pressed[pygame.K_LEFT]: x -= 1
-        if pressed[pygame.K_RIGHT]: x += 1
-        
-        if is_blue:
-            color = (0, 128, 255)            
-        else: 
-            color = (255, 100, 0)
-        color = colorRgb
-        pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
-        
-        pygame.display.flip()
+
+background = pygame.image.load("images/clock.png")
+
+background = pygame.transform.scale(background, (width, height))
+
+center_x = width // 2
+center_y = height // 2
+second_hand_length = min(width, height) // 3
+minute_hand_length = min(width, height) // 3.5
+hour_hand_length = min(width, height) // 4.5
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    current_time = time.localtime()
+    seconds = current_time.tm_sec
+    minutes = current_time.tm_min
+
+
+    second_angle = math.radians(seconds * 6 - 90)  
+    minute_angle = math.radians(minutes * 6 + seconds * 0.1 - 90)  
+
+    screen.fill((255, 255, 255))
+    
+    screen.blit(background, (0, 0))
+    
+    minute_x = center_x + minute_hand_length * math.cos(minute_angle)
+    minute_y = center_y + minute_hand_length * math.sin(minute_angle)
+    pygame.draw.line(screen, (0, 0, 0), (center_x, center_y), (minute_x, minute_y), 4)
+    
+
+    second_x = center_x + second_hand_length * math.cos(second_angle)
+    second_y = center_y + second_hand_length * math.sin(second_angle)
+    pygame.draw.line(screen, (255, 0, 0), (center_x, center_y), (second_x, second_y), 4)
+    
+    pygame.draw.circle(screen, (0, 0, 0), (center_x, center_y), 6)
+    
+
+    pygame.display.flip()
+    
+    time.sleep(0.1)
+
+pygame.quit()
