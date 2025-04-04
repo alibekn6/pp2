83bfbdce5cf1280e3b537002c034d49ad93fe161
alibekn6 <alibekandanuarbek@gmail.com>
2025-04-04 23:45:57 +0500

    racer

--- python_tutor_/lab8/game.py
+++ python_tutor_/lab8/game.py
@@ -1,103 +1,181 @@
+# Imports
+import sys
 import pygame
 from pygame.locals import *
+import random, time
 
+
+# Init
 pygame.init()
 
-WHITE =     (255, 255, 255)
-BLUE =      (  0,   0, 255)
-GREEN =     (  0, 255,   0)
-RED =       (255,   0,   0)
 FPS = 60
+FramePerSec = pygame.time.Clock()
+# Predefined some colors
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-DISPLAYSURF = pygame.display.set_mode((700,700))
-pygame.display.set_caption("Xolst")
-DISPLAYSURF.fill(WHITE)
+# Variables
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+COINSCORE = 0
 
-clock = pygame.time.Clock()
+# Setting up FONTS
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
 
-drawing = False
-last_pos = None
-CURRENT_COLOR = BLUE  # Default drawing color
-CURRENT_PEN_SIZE = 5 # default size of pen
+background = pygame.image.load("assets/AnimatedStreet.png")
 
-clear_button_rect = pygame.Rect(5, 5, 100, 50)
-erase_button_rect = pygame.Rect(120, 5, 100, 50)
-
-
-def clear_button():
-    font = pygame.font.Font(None, 40)  
-
-    button_color = BLUE
-    text_surface = font.render("clear", True, WHITE)
-    text_rect = text_surface.get_rect(center=clear_button_rect.center)
-
-
-    pygame.draw.rect(DISPLAYSURF, button_color, clear_button_rect, border_radius=10)
-    DISPLAYSURF.blit(text_surface, text_rect)
-
-def erase_button():
-    font = pygame.font.Font(None, 40)  
-
-    button_color = RED if CURRENT_COLOR == WHITE else BLUE
-
-    text_surface = font.render("erase", True, WHITE)
-    text_rect = text_surface.get_rect(center=erase_button_rect.center)
-
-
-    pygame.draw.rect(DISPLAYSURF, button_color, erase_button_rect, border_radius=10)
-    DISPLAYSURF.blit(text_surface, text_rect)
+# Creating a white screen
+DISPLAYSURFACE = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURFACE.fill(WHITE)
+pygame.display.set_caption("Gameeee")
 
 
 
 
-def draw(event):
-
-    global drawing, last_pos
-    if event.type == MOUSEBUTTONDOWN:
-        if not (clear_button_rect.collidepoint(event.pos) or erase_button_rect.collidepoint(event.pos)):
-            drawing = True
-            last_pos = event.pos
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/coin.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)
     
-    if event.type == MOUSEBUTTONUP:
-        drawing = False
-        last_pos = None
+    def move(self):
+        self.rect.move_ip(0, SPEED)
+        if (self.rect.top > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
-    if event.type == MOUSEMOTION and drawing:
-        current_pos = event.pos
-        if last_pos:
-            pygame.draw.line(DISPLAYSURF, CURRENT_COLOR, last_pos, current_pos, CURRENT_PEN_SIZE)
-        last_pos = current_pos
-
-
+    def reset(self):
+        self.rect.top = 0
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
 
+
+# Enemy class (Car)
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/Enemy.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)
+
+    def move(self):
+        global SCORE
+        self.rect.move_ip(0, SPEED)
+        if (self.rect.bottom > 800):
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+
+# Player class (Car that we drive)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('assets/Player.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+    
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+             self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+             self.rect.move_ip(0,5)
+         
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-10, 0)
+        
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(10, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)  
+
+
+
+# Object of classes
+P = Player()
+E = Enemy()
+C = Coin()
+
+# adding them to a group
+enemies = pygame.sprite.Group()
+enemies.add(E)
+
+coins = pygame.sprite.Group()
+coins.add(C)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P)
+all_sprites.add(E)
+all_sprites.add(C)
+
+# adding a new user event (unique id)
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+# every 1000 milisecond this event accurs -> which increases the speed
+
+# main game loop
 while True:
-    
-    clear_button()
-    erase_button()
-
+    # for loop through all events accuring
     for event in pygame.event.get():
-        draw(event)
 
+        if event.type == INC_SPEED:
+            SPEED += 0.5
+        
         if event.type == QUIT:
             pygame.quit()
+            sys.exit()
+
+    # pygame.mixer.Sound('assets/background.wav').play()
+
+
+    # setting up the background 
+    DISPLAYSURFACE.blit(background, (0,0))
+    # showing score in the screen
+    scores = font_small.render(str(SCORE), True, BLACK)
+    DISPLAYSURFACE.blit(scores, (10, 10))
+    scores = font_small.render(f"Coins: {COINSCORE}", True, BLACK)
+    DISPLAYSURFACE.blit(scores, (SCREEN_WIDTH-100,10))
+    # moves and redraws all sprites
+    for entity in all_sprites:
+        DISPLAYSURFACE.blit(entity.image, entity.rect)
+        entity.move()
+    
+
+    # checking if collision occurs
+    if pygame.sprite.spritecollideany(P, enemies):
+        pygame.mixer.Sound('assets/crash.wav').play()
+        time.sleep(0.5)
         
-        if event.type == MOUSEBUTTONDOWN:
-            if clear_button_rect.collidepoint(event.pos):
-                print("BUTTON CLIKEDD!!!!")
-                DISPLAYSURF.fill(WHITE)
-            
-            if erase_button_rect.collidepoint(event.pos):
-                if CURRENT_COLOR == BLUE:
-                    CURRENT_COLOR = WHITE
-                    CURRENT_PEN_SIZE = 20
-                    print("Erase mode on")
-                else:
-                    CURRENT_COLOR = BLUE
-                    CURRENT_PEN_SIZE = 5
-                    print("DRAW MODE ON")
-                
+        DISPLAYSURFACE.fill(RED)
+        DISPLAYSURFACE.blit(game_over, (30, 250))
+    
+        pygame.display.update()
+
+        # if accurs deleting entities from all_sprites
+        for entity in all_sprites:
+            entity.kill()
+
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
+    if pygame.sprite.spritecollideany(P, coins):
+        COINSCORE += 1
+        C.reset()
 
 
     pygame.display.update()
-    clock.tick(FPS)
+    FramePerSec.tick(FPS)
