9b364567a8d5897cc61a8bbf420f29d8a5ccdf0c
alibekn6 <alibekandanuarbek@gmail.com>
2025-04-17 17:16:32 +0500

    feat: lab11 finished

--- python_tutor_/lab11/task.py
+++ python_tutor_/lab11/task.py
@@ -1,10 +1,11 @@
 import psycopg2
 import csv
-
 import os
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
@@ -19,41 +20,119 @@ def connect():
         user=DB_USER,
         password=DB_PASSWORD,
         host=DB_HOST
-)
+    )
 
 
-def search_by_pattern():
-    pattern = input("enter pattern: ")
+def create_table():
     conn = connect()
     cur = conn.cursor()
-
-    cur.execute("SELECT * FROM search_users(%s);", (pattern,))
-    results = cur.fetchall()
-
-    if results:
-        print("results")
-        for row in results:
-            print(row)
-
-    else:
-        print("no match found (")
+    cur.execute("""
+        CREATE TABLE phonebook (
+            id SERIAL PRIMARY KEY,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50),
+            phone VARCHAR(15) UNIQUE NOT NULL
+        );
+    """)
+    conn.commit()
     cur.close()
     conn.close()
+    print("success!")
 
 
-def insert_or_update_user():
-    fname = input("Enter first name: ")
-    lname = input("Enter last name: ")
+def insert_from_csv():
+    csv_filename = input("Enter CSV file path: ")
+    try:
+        conn = connect()
+        cur = conn.cursor()
+        with open(csv_filename, "r") as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header
+            for row in reader:
+                cur.execute("""
+                    INSERT INTO phonebook (first_name, last_name, phone)
+                    VALUES (%s, %s, %s)
+                    ON CONFLICT (phone) DO NOTHING;
+                """, row)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("uploaded successfully from csv")
+    except Exception as e:
+        print(e)
+
+
+
+def insert_user():
+    fname = input("Enter first Name: ")
+    lname = input("Enter last Name: ")
     phone = input("Enter phone number: ")
 
     conn = connect()
     cur = conn.cursor()
     try:
-        cur.execute("CALL insert_or_update_user(%s, %s, %s);", (fname, lname, phone))
+        cur.execute("""
+            INSERT INTO phonebook (first_name, last_name, phone)
+            VALUES (%s, %s, %s)
+            ON CONFLICT (phone) DO NOTHING;
+        """, (fname, lname, phone))
         conn.commit()
-        print("User inserted or updated.")
-    except Exception as e:
-        print("Error:", e)
+        print("User added")
+    except psycopg2.Error as e:
+        print(e)
+    cur.close()
+    conn.close()
+
+
+
+def update_phone():
+    old_phone = input("Enter old phone number: ")
+    new_phone = input("Enter new phone number: ")
+
+    conn = conn()
+    cur = conn.cursor()
+    cur.execute("UPDATE phonebook SET phone = %s WHERE phone = %s;", (new_phone, old_phone))
+    if cur.rowcount > 0:
+        print("updated successfully!")
+    else:
+        print("Phone number not found")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+def search_user():
+    search_term = input("Enter name or phone to search: ")
+
+    conn = connect()
+    cur = conn.cursor()
+    query = "SELECT * FROM phonebook WHERE first_name ILIKE %s OR last_name ILIKE %s OR phone ILIKE %s;"
+    cur.execute(query, (f"%{search_term}%", f"%{search_term}%", f"%{search_term}%"))
+    results = cur.fetchall()
+    
+    if results:
+        print("\nResults: ")
+        for row in results:
+            print(row)
+    else:
+        print("Not found")
+
+    cur.close()
+    conn.close()
+
+def delete_user():
+    username = input("Enter usernmae delete: ")
+
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute("DELETE FROM phonebook WHERE first_name = %s;", (username))
+    
+    if cur.rowcount > 0:
+        print("User deleted")
+    else:
+        print("user not found")
+    
+    conn.commit()
     cur.close()
     conn.close()
 
@@ -61,38 +140,194 @@ def insert_or_update_user():
 
 
 
-def insert_multiple_users():
-    from ast import literal_eval
 
-    raw_input = input("Enter users as [(fname, lname, phone), ...]: ")
-    try:
-        users = literal_eval(raw_input)  # Safely parse string into list of tuples
-        first_names = [u[0] for u in users]
-        last_names = [u[1] for u in users]
-        phones = [u[2] for u in users]
+def search_by_pattern():
+    pattern = input("enter part of first name, last name or phone: ")
+    sql = """
+        SELECT id, first_name, last_name, phone FROM phonebook
+        WHERE first_name ILIKE %s OR last_name  ILIKE %s OR phone ILIKE %s ORDER BY id;
+    """
+    conn = connect()
+    cur  = conn.cursor()
+    cur.execute(sql, (f"%{pattern}%", f"%{pattern}%", f"%{pattern}%"))
+    rows = cur.fetchall()
+    if rows:
+        print("\nMatches:")
+        for r in rows:
+            print(r)
+    else:
+        print("no matches found (")
+    cur.close()
+    conn.close()
 
-        conn = connect()
-        cur = conn.cursor()
 
-        cur.execute("SELECT * FROM insert_multiple_users(%s, %s, %s);", (first_names, last_names, phones))
-        invalids = cur.fetchall()
 
-        if invalids:
-            print("Invalid or failed inserts:")
-            for row in invalids:
-                print(row)
-        else:
-            print("All users inserted successfully.")
 
+
+def insert_or_update_user():
+    fname = input("first name: ")
+    lname = input("last name: ")
+    phone = input("phone number: ")
+
+    conn = connect()
+    cur  = conn.cursor()
+
+    cur.execute(
+        "SELECT id FROM phonebook WHERE first_name=%s AND last_name=%s;",
+        (fname, lname)
+    )
+    exists = cur.fetchone()
+    if exists:
+        cur.execute(
+            "UPDATE phonebook SET phone=%s WHERE id=%s;",
+            (phone, exists[0] )
+        )
+        print("phone updated: ", fname, lname)
+    else:
+        cur.execute(
+            "INSERT INTO phonebook(first_name, last_name, phone) VALUES (%s, %s, %s);",
+            (fname, lname, phone)
+        )
+        print("user added : ", fname, lname)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+
+
+# inserting many users
+def insert_many_users():
+    n = int(input("how many users to add ? "))
+    invalids = []
+    conn = connect()
+    cur  = conn.cursor()
+
+    for i in range(n):
+        fname = input("first name: ")
+        lname = input("last name: ")
+        phone = input("phone: ")
+        # starts with + , 10-15 digits
+        if not re.match(r'^\+?\d{10,15}$', phone):
+            invalids.append((fname, lname, phone))
+            continue
+
+        
+        cur.execute(
+            """
+            INSERT INTO phonebook(first_name, last_name, phone)
+            VALUES (%s, %s, %s)
+            """,
+            (fname, lname, phone)
+        )
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    if invalids:
+        print("\ninvalid : ")
+        for i in invalids:
+            print(" ", i)
+    else:
+        print("\nall contacts added")
+
+
+def paginated_query():
+    limit  = int(input("Limit per page: "))
+    offset = int(input("Offset (rows to skip): "))
+    sql = """
+        SELECT id, first_name, last_name, phone
+        FROM phonebook
+        ORDER BY id
+        LIMIT %s OFFSET %s;
+    """
+    conn = connect()
+    cur  = conn.cursor()
+    cur.execute(sql, (limit, offset))
+    rows = cur.fetchall()
+    if rows:
+        for r in rows:
+            print(r)
+    else:
+        print("no rows in this range (")
+    cur.close()
+    conn.close()
+
+
+
+# deleting user data
+def delete_user_data():
+    choice = input("delete by name or phone? (n, p) ").lower()
+    conn = connect()
+    cur  = conn.cursor()
+
+    if choice == 'n':
+        name = input("enten your first name: ")
+        cur.execute("DELETE FROM phonebook WHERE first_name=%s;", (name,))
+    elif choice == 'p':
+        phone = input("enter your phone: ")
+        cur.execute("DELETE FROM phonebook WHERE phone=%s;", (phone,))
+    else:
+        print("invalid choice ( ")
         cur.close()
         conn.close()
-    except Exception as e:
-        print("Error:", e)
+        return
+
+    if cur.rowcount:
+        print("deleted", cur.rowcount, "records.")
+    else:
+        print("No matching record found.")
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
+# CLI Menu
+def main():
+    # create_table() # already created in dev (local)
+    while True:
+        print("\nPHONEBOOK MENU")
+        print("1 Insert from CSV")
+        print("2 Insert manually")
+        print("3 Update phone number")
+        print("4 Search contacts")
+        print("5 Delete contact")
+        print("6 Search by pattern")
+        print("7 Insert or update if exist")
+        print("8 Insert many users")
+        print("9 Paginated query")
+        print("10 Delete user data")
+        print("11 Exit")
 
-insert_multiple_users()
-# seeds 
-"""
-[("John", "Doe", "+1234567890"),("Jane", "Smith", "+1987654321"),("Alice", "Johnson", "+1122334455"),("Bob", "Williams", "+9988776655")]
-"""
+        choice = input("Choose (1-6): ")
+
+        if choice == "1":
+            insert_from_csv()
+        elif choice == "2":
+            insert_user()
+        elif choice == "3":
+            update_phone()
+        elif choice == "4":
+            search_user()
+        elif choice == "5":
+            delete_user()
+        elif choice == "6":
+            search_by_pattern()
+        elif choice == "7":
+            insert_or_update_user()
+        elif choice == "8":
+            insert_many_users()
+        elif choice == "9":
+            paginated_query()
+        elif choice == "10":
+            delete_user_data()
+        elif choice == "11":
+            print("bye!")
+            break
+        else:
+            print("try again!")
+
+
+main()
